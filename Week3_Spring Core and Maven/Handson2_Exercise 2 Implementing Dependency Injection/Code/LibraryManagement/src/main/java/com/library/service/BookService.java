bb8b1e50ed2a4bfc7d6e385cbe8fee4832ee0276
shankarrs23 <shankarrs2307@gmail.com>
2025-07-06 19:37:51 +0530

    Add files via upload

---
+++ Week3_Spring Core and Maven/Handson2_Exercise 2 Implementing Dependency Injection/Code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+public class BookService {
+    private BookRepository bookRepository;
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public void displayBooks() {
+        System.out.println("Calling the Service");
+        bookRepository.getAllBooks();
+    }
+}
---
+++ Week3_Spring Core and Maven/Handson4_Creating and Configuring a Maven Project/Code/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,8 @@
+package com.library;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        System.out.println("Dependencies and Plugins are added Successfully");
+
+    }
+}
